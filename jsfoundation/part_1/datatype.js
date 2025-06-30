8684f3a0c21c14555182e941951343973a9b8521
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-06-30 11:37:32 +0530

    datatypes in JS

---
+++ jsfoundation/part_1/datatype.js
@@ -0,0 +1,32 @@
+const { __unstable__loadDesignSystem } = require("tailwindcss");
+
+console.log("hello");
+
+console.log("chai")
+
+/*
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+*/
+
+
+let score = 102
+
+let name = "Hanan"
+
+let isLoggedIn = true
+
+//object
+let teaTypes = ["milk", "lemon", "orange", "oolong"]
+
+let user = {firstname: "Muhammad", middlename: "Hanan", lastname: "Sajid"}
+
+console.table(user)
+console.log(score);
---
+++ jsfoundation/part_1/printing.js
@@ -0,0 +1,6 @@
+console.log("hello web dev");
+
+
+console.table({hanan: 19}, {harun: 12}, {sajid: 52});
+
+
