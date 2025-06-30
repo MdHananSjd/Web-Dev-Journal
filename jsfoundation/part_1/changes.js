c519d8e0ea154198542ceaefa0950e998c2da854
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-06-30 11:52:53 +0530

    working with operations in JS

---
+++ jsfoundation/part_1/changes.js
@@ -0,0 +1,7 @@
+let gameName = "spiderman";
+gameName = "superman";
+console.log(gameName);
+
+const username = "badlad"
+username = "hanan"
+console.log(username); //TypeError cant change a const variable
---
+++ jsfoundation/part_1/operations.js
@@ -0,0 +1,20 @@
+
+let score = 102;
+let bonus = 25;
+let totalScore = score + bonus;
+
+console.log(totalScore);
+
+let addition = 4 + 5
+let subtract = 9 - 3
+let mult = 4 * 3
+let divi = 10/5
+
+console.log(addition);
+console.log(subtract);
+console.log(mult);
+console.log(divi);
+
+BigInt number = 2**12212312
+
+console.log(number);
