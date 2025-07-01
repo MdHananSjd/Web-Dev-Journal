370262e390615deb2b9fcaa6c2c6c7bc3f39e9ad
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-07-01 20:38:16 +0530

    arrays in JS

--- jsfoundation/part_4/arrays.js
+++ jsfoundation/part_4/arrays.js
@@ -1,5 +1,23 @@
 let teaFlavours = ['green', 'black', 'oolong']
 const firstTea = teaFlavours[0]
 
-lastTea = teaFlavours.pop()
-console.log(lastTea);
+
+let softTea = teaFlavours; //soft copy
+
+// let hardTea = [...teaFlavours] // hard copy
+let hardTea = teaFlavours.slice() // hard copy
+
+// teaFlavours.pop()
+// console.log(softTea);
+// console.log(hardTea);
+
+let coffeeFlavours = ['espresso', 'black', 'latte']
+
+let merged = teaFlavours.concat(coffeeFlavours)
+// console.log(merged);
+
+let menuLength = merged.length
+// console.log(menuLength);
+
+let inList = merged.includes('latte')
+console.log(inList);
