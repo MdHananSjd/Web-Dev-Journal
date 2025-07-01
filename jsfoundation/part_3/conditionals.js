e37b597ce8a651441fa10a0139ac1a23952e05da
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-07-01 17:53:50 +0530

    conditionals update

---
+++ jsfoundation/part_3/conditionals.js
@@ -0,0 +1,25 @@
+let num1 = 5
+let num2 = 8
+
+if (num1 == num2) {
+    console.log("yeah theyre equal");
+}else{
+    console.log("nah theyre not the same");
+    
+}
+
+if (typeof num1 === 'number') {
+    console.log("yeah this is a number");
+    
+}
+let bool = false
+
+if (bool) {
+    console.log(
+        "Yeah thats right"
+    );
+    
+}else{
+    console.log("thats false");
+    
+}
