391516655b111724ec6c188088a3039ac0ca3a45
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-07-01 17:03:16 +0530

    updates

---
+++ jsfoundation/part_2/non_primitives.js
@@ -0,0 +1,32 @@
+const username = {
+    "first name": "hanan",
+    isLoggedIn: true
+}
+
+username.firstname = "mhs"
+username.lastname = "sajid"
+
+// console.log(username["first name"]);
+// console.log(typeof username);
+// console.log(username.lastname);
+
+let today = new Date();
+// console.log(today.getDate(), today.getDay());
+
+// if(1 > 0){
+//     console.log(today.getMonth());
+    
+// }
+
+let heroes = ["superman", 'spiderman', 'hulk', true]
+
+// for (let i=0 ; i<4 ; i++){
+//     if(typeof heroes[i] == "string"){
+//         console.log(heroes[i]);
+//     }else{
+//         console.log(`The value is ${heroes[i]}`);
+//     }
+// }
+
+let isValue = true;
+console.log(isValue + 5);
