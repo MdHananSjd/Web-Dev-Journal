5f2f5f4fa94815e7992f5ab522f273ee8a39534a
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-07-03 13:38:10 +0530

    updates

---
+++ jsfoundation/part_5/functions.js
@@ -0,0 +1,18 @@
+function makeTea(typeOfTea){
+    let result = `Making ${typeOfTea}!`
+    console.log(result);
+    
+}
+
+// makeTea('orange tea')
+
+function orderTea(teaType){
+    let tea = teaType
+    function confirmOrder(){
+        console.log(`Order confirmed for ${tea}`);
+    }
+
+    return confirmOrder()
+}
+
+orderTea("earl grey")
