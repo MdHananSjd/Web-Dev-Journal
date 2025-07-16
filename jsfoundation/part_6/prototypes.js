bc1fdb22c56b4b408b428e60ea35c122e3ed2609
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-07-16 12:49:41 +0530

    minute updates

--- jsfoundation/part_6/prototypes.js
+++ jsfoundation/part_6/prototypes.js
@@ -1,3 +1,10 @@
 let computer = {cpu:12}
+let lenovo = {
+    screen: "HD",
+    __proto__: computer
+}
 
+let tomHardware = {}
 console.log(`computer`, computer.__proto__);
+console.log(`lenovo`, lenovo.__proto__, lenovo.screen);
+
